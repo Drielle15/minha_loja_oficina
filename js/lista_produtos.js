7a6e7bf4ca1ef10b3b19d908a78f18447956e0e7
Drielle Dantas <drielle.dantas@aluno.senai.br>
2026-07-09 16:44:40 -0300

    criado o arquivos lista_produtos.js e produtos.js

---
+++ js/lista_produtos.js
@@ -0,0 +1,140 @@
+
+const produtos = [
+    {
+        id_produto: 1,
+        descricao_produto: "Torno de Bancada (Morsa de Ferro)",
+        valor_unitario: 292.34,
+        unidade: "UN",
+        caminho_imagem: "../imagens/Torno de Bancada (Morsa de Ferro).png",
+        id_secao: 1,
+        secao: "Ferramentas"
+    },
+    {
+        id_produto: 2,
+        descricao_produto: "HEXX PREMIUM SYNT 5W30",
+        valor_unitario: 46.55,
+        unidade: "UN",
+        caminho_imagem: "../imagens/HEXX PREMIUM SYNT 5W30.png",
+        id_secao: 2,
+        secao: "Lubrificantes"
+    },
+    {
+        id_produto: 3,
+        descricao_produto: "Macaco Hidráulico Tipo Jacaré",
+        valor_unitario: 1491.18,
+        unidade: "UN",
+        caminho_imagem: "../imagens/Macaco Hidráulico Tipo Jacaré.png",
+        id_secao: 1,
+        secao: "Ferramentas"
+    },
+    {
+        id_produto: 4,
+        descricao_produto: "Carrinho de Ferramentas de Rodinha (Gabinete)",
+        valor_unitario: 909.90,
+        unidade: "UN",
+        caminho_imagem: "../imagens/Carrinho de Ferramentas de Rodinha (Gabinete).png",
+        id_secao: 1,
+        secao: "Ferramentas"
+    },
+    {
+        id_produto: 5,
+        descricao_produto: "Disco de Freio de Alta Performance",
+        valor_unitario: 290.00,
+        unidade: "UN",
+        caminho_imagem: "../imagens/Disco de Freio de Alta Performance.png",
+        id_secao: 3,
+        secao: "Freios"
+    },
+    {
+        id_produto: 6,
+        descricao_produto: "Chave de Impacto Pneumática (Pistola de Ar)",
+        valor_unitario: 715.87,
+        unidade: "UN",
+        caminho_imagem: "../imagens/Chave de Impacto Pneumática (Pistola de Ar).png",
+        id_secao: 1,
+        secao: "Ferramentas"
+    },
+    {
+        id_produto: 7,
+        descricao_produto: "Scanner de Diagnóstico Automotivo OBD2",
+        valor_unitario: 159.00,
+        unidade: "UN",
+        caminho_imagem: "../imagens/Scanner de Diagnóstico Automotivo OBD2.png",
+        id_secao: 4,
+        secao: "Diagnóstico"
+    },
+    {
+        id_produto: 8,
+        descricao_produto: "Painel de Ferramentas Perfurado",
+        valor_unitario: 339.41,
+        unidade: "UN",
+        caminho_imagem: "../imagens/Painel de Ferramentas Perfurado.png",
+        id_secao: 1,
+        secao: "Ferramentas"
+    },
+    {
+        id_produto: 9,
+        descricao_produto: "Óculos de Proteção",
+        valor_unitario: 21.70,
+        unidade: "UN",
+        caminho_imagem: "../imagens/Óculos de Proteção.png",
+        id_secao: 5,
+        secao: "Segurança"
+    },
+    {
+        id_produto: 10,
+        descricao_produto: "Vela de Ignição",
+        valor_unitario: 98.99,
+        unidade: "UN",
+        caminho_imagem: "../imagens/Vela de ignição.png",
+        id_secao: 6,
+        secao: "Motor"
+    },
+    {
+        id_produto: 11,
+        descricao_produto: "Elevador Automotivo Hidráulico",
+        valor_unitario: 8899.90,
+        unidade: "UN",
+        caminho_imagem: "../imagens/Elevador Automotivo Hidráulico.png",
+        id_secao: 1,
+        secao: "Ferramentas"
+    },
+    {
+        id_produto: 12,
+        descricao_produto: "Amortecedor de Suspensão Coilover",
+        valor_unitario: 4665.57,
+        unidade: "UN",
+        caminho_imagem: "../imagens/Amortecedor de Suspensão Coilover 40.png",
+        id_secao: 7,
+        secao: "Suspensão"
+    },
+    {
+        id_produto: 13,
+        descricao_produto: "Bateria Automotiva Duran",
+        valor_unitario: 643.08,
+        unidade: "UN",
+        caminho_imagem: "../imagens/Bateria Automotiva Duran.png",
+        id_secao: 8,
+        secao: "Elétrica"
+    },
+    {
+        id_produto: 14,
+        descricao_produto: "Filtro Ar Esportivo Cônico Duplo",
+        valor_unitario: 133.00,
+        unidade: "UN",
+        caminho_imagem: "../imagens/Filtro Ar Esportivo Cônico Duplo.png",
+        id_secao: 6,
+        secao: "Motor"
+    },
+    {
+        id_produto: 15,
+        descricao_produto: "Jogo de Chaves de Boca e Estrela (Chaves Combinadas)",
+        valor_unitario: 140.90,
+        unidade: "UN",
+        caminho_imagem: "../imagens/Jogo de Chaves de Boca e Estrela (Chaves Combinadas).png",
+        id_secao: 1,
+        secao: "Ferramentas"
+    }
+];
+
+   export{produtos}
